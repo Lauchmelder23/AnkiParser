2c87c93efc5f50dc253a1b4471222159ba271553
Robert <robert.trololo@gmail.com>
2020-12-14 13:31:42 +0100

    Changed error handling

--- examples/test.cpp
+++ examples/test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <Package.hpp>
+#include <zip.h>
 
 int main(int argc, char** argv)
 {
@@ -9,13 +10,13 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-	try 
+	Anki::Package pkg;
+	int result = pkg.Open(argv[1]);
+	if (result)
 	{
-		Anki::Package pkg(argv[1]);
-	}
-	catch (const char* e)
-	{
-		std::cerr << e << std::endl;
+		zip_error err;
+		zip_error_init_with_code(&err, result);
+		std::cerr << "Could not open package: " << zip_error_strerror(&err) << " (" << result << ")" << std::endl;
 	}
 
 	return 0;
